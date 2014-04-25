f26aeea8eb9b02bb4099683b6a0767fa786604df
Quan Hoang <qnhoang81@ubuntu.(none)>
2014-04-24 18:29:44 -0700

    This is a shell for team vendor folder.
    Change-Id: Id34aaf656b2a949f50b84fcc16f483f81287838a

---
+++ configs/common.mk
@@ -0,0 +1,29 @@
+ifeq ($(PRODUCT_GMS_CLIENTID_BASE),)
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.com.google.clientidbase=android-google
+else
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.com.google.clientidbase=$(PRODUCT_GMS_CLIENTID_BASE)
+endif
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.url.legal=http://www.google.com/intl/%s/mobile/android/basic/phone-legal.html \
+    ro.url.legal.android_privacy=http://www.google.com/intl/%s/mobile/android/basic/privacy.html \
+    ro.com.android.wifi-watchlist=GoogleGuest \
+    ro.error.receiver.system.apps=com.google.android.feedback \
+    ro.com.google.locationfeatures=1 \
+    ro.setupwizard.enterprise_mode=1 \
+    ro.kernel.android.checkjni=0 \
+    persist.sys.root_access=3
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.build.selinux=1
+
+PRODUCT_COPY_FILES += packages/wallpapers/LivePicker/android.software.live_wallpaper.xml:/system/etc/permissions/android.software.live_wallpaper.xml
+
+# World APNs
+PRODUCT_COPY_FILES += \
+    vendor/teamtek/prebuilt/common/etc/apns-conf.xml:system/etc/apns-conf.xml
+
+# Version information
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_VERSION_TAGS=release-keys USER=android-build BUILD_UTC_DATE=$(shell date +"%s")
